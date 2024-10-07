1d4818a8978c77c07b6aba2df6b2b7ed1a916ecf
Filipe <filipe.raitz@hotmail.com>
2024-10-07 11:47:16 -0400

    criando hero e importando fonte

--- gulpfile.js
+++ gulpfile.js
@@ -20,5 +20,5 @@ exports.default = gulp.parallel(styles, images);
 
 
 exports.watch = function() {
-    gulp.watch('.src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
